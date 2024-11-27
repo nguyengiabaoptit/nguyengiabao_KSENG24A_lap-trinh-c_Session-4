b8462c9f477d6f6068cd9f8297d2dabc61ec52c0
nguyengiabaoptit <nguyengiabao2880@gmail.com>
2024-11-27 13:19:59 +0700

    upload bài 1

---
+++ bài 1/Untitled1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	printf("moi ban nhap so vao:");
+	scanf("%d", &a);
+	if(a > 0){
+	printf("day la so duong");
+	}else{
+	printf("day la so am");
+	}
+	
+	return 0;
+}
